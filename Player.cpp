515e8c028966d8196027ab0aac39e82bf82ebe3f
Matias Viglione <matiasericvig@gmail.com>
2016-04-01 14:56:57 +0200

    Completacion del sistema de sprites

--- Player.cpp
+++ Player.cpp
@@ -3,17 +3,25 @@
 Player::Player(sf::RenderWindow &window, sf::Vector2f &camera) :
 	Entity(window, camera)
 {
-	shape.setFillColor(sf::Color::Green);
 	if(!texture.loadFromFile("player.png"));
 	sprite.setTexture(texture);
+	frame = 0;
+	frames = {sf::IntRect(0,0,60,60), sf::IntRect(60,0,71,60),sf::IntRect(0,0,60,60),sf::IntRect(131,0,71,60)};
+}
+
+void Player::nextFrame() {
+    if (frame == frames.size() - 1) {
+        frame = 0;
+    } else {
+        frame++;
+    }
 }
 
 void Player::draw() {
     sprite.setPosition(position + sf::Vector2f(30,30) + camera);
 	sf::Vector2f curPos = sprite.getPosition();
     sf::Vector2i position = sf::Mouse::getPosition(window);
-
-
+    sprite.setTextureRect(frames[frame]);
 
     const float PI = 3.14159265;
 
@@ -23,8 +31,29 @@ void Player::draw() {
     float rotation = (atan2(dy, dx)) * 180 / PI;
 
 
-    sprite.setOrigin(30,30);
+    sprite.setOrigin(50,30);
 	sprite.setRotation(rotation);
 	window.draw(sprite);
+}
+
+void Player::update() {
+	sf::Time time_now = clock.getElapsedTime();
+	if (lastUpdated.asMicroseconds() == 0) {
+		// El primer frame del joc ningú es mou
+		lastUpdated = time_now;
+		return;
+	}
+	long long micros = time_now.asMicroseconds() - lastUpdated.asMicroseconds();
+	const float vel = 0.0003; // pixels / ms
+	//position += moving * vel * micros;
+	target_movement = moving * float(vel * micros);
+	lastUpdated = clock.getElapsedTime();
+
+    if (target_movement == sf::Vector2f(0, 0)) {
+        frame = 0;
+    } else if ((time_now - lastAnimFrame).asMilliseconds() > 300) {
+        nextFrame();
+        lastAnimFrame = time_now;
+	}
 }
 
--- Player.hpp
+++ Player.hpp
@@ -8,10 +8,15 @@ class Player : public Entity
 	public:
 		Player(sf::RenderWindow &window, sf::Vector2f &camera);
 		void draw();
+		void update();
 	private:
 		sf::CircleShape shape{player_r};
 		sf::Texture texture;
 		sf::Sprite sprite;
+		void nextFrame();
+		std::vector<sf::IntRect> frames;
+		int frame;
+		sf::Time lastAnimFrame;
 };
 
 #endif /* !PLAYER_H */
