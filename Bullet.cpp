ee788958301578f54ff41b21af5f5780fb2e9986
Jaume Delclòs Coll <cosa.rara97@gmail.com>
2016-05-19 19:46:15 +0200

    faster bullets

--- Bullet.cpp
+++ Bullet.cpp
@@ -4,7 +4,7 @@
 #include "vector.hpp"
 
 Bullet::Bullet(sf::Clock *clock, sf::RenderWindow *window, sf::Vector2f *camera) :
-	Entity(clock, window, camera, 0.0006f /*vel*/, 5 /*radius*/),
+	Entity(clock, window, camera, 0.001f /*vel*/, 5 /*radius*/),
 	shape(radius)
 {
 	shape.setFillColor(sf::Color::Black);
